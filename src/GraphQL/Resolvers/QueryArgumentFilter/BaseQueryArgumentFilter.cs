d540c831d20b2dad0a016dc9abe6f990d68e5c2c
Toby Caulk <tcaulk@mainsequence.net>
2019-07-30 15:30:14 -0400

    Make In/Nin filters list types

--- src/GraphQL/Resolvers/QueryArgumentFilter/BaseQueryArgumentFilter.cs
+++ src/GraphQL/Resolvers/QueryArgumentFilter/BaseQueryArgumentFilter.cs
@@ -8,15 +8,15 @@ namespace GraphQL.Resolvers.QueryArgumentFilter
     {
         public TSourceType Eq { get; set; }
         public TSourceType Ne { get; set; }
-        public TSourceType In { get; set; }
-        public TSourceType Nin { get; set; }
+        public List<TSourceType> In { get; set; }
+        public List<TSourceType> Nin { get; set; }
 
         public BaseQueryArgumentFilter()
         {
             Field<TSourceType>(name: "Eq");
             Field<TSourceType>(name: "Ne");
-            Field<TSourceType>(name: "In");
-            Field<TSourceType>(name: "Nin");
+            Field<ListGraphType<TSourceType>>(name: "In");
+            Field<ListGraphType<TSourceType>>(name: "Nin");
         }
     }
 }
